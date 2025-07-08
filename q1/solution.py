25a03fbc3f30182b989bc51e93ecbf4a8bee1d1e
Minahil02 <zayankhan0910@gmail.com>
2025-07-08 21:34:45 +0500

    Moved solution.py into q1 folder

--- q1/solution.py
+++
@@ -1,7 +0,0 @@
-#1
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-marks = int(input("Enter your marks: "))
-print(name, type(name))
-print(age, type(age))
-print(marks, type(marks))
